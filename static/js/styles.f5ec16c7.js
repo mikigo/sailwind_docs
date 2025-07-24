d35f314b203e0b3de31bf4761fdf25b1757f40b9
mikigo <mikigo@users.noreply.github.com>
2025-07-24 08:11:16 +0000

    deploy: 1f3edb79936d7347c9d858a06efde3be53bb386a

---
+++ static/js/styles.f5ec16c7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkSailWind_Docs=self.webpackChunkSailWind_Docs||[]).push([["6212"],{498:function(){},3269:function(){},9752:function(){},6771:function(){},7329:function(){},8632:function(){}}]);
